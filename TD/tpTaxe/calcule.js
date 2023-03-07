03c2f96fa968eaa866b8da8ec7e152fcf78bbd94
khalil <khalillakhdharatc@gmail.com>
2023-03-07 13:50:06 +0100

    Update calcule.js

--- TD/tpTaxe/calcule.js
+++ TD/tpTaxe/calcule.js
@@ -1,14 +1,19 @@
-function calculeTaxe()
-{
-    let taxe =0;
-    let titre=document.getElementById("titre").value;
-    let prix=document.getElementById("prix").value;
-    let quantite=document.getElementById("quantite").value;
-    let pTotale=prix*quantite;
+function calculeTaxe() {
+  let taxe = 0
+  let pFinale = 0
+  let titre = document.getElementById('titre').value
+  let prix = document.getElementById('prix').value
+  let quantite = document.getElementById('quantite').value
+  let pTotale = prix * quantite
 
-    //alert(`produit: ${titre} and prix: ${prix} and quantite: ${quantite}`);
-console.log(`produit: ${titre} and prix: ${prix} and quantite: ${quantite}`);
+  //alert(`produit: ${titre} and prix: ${prix} and quantite: ${quantite}`);
+  console.log(`produit: ${titre} and prix: ${prix} and quantite: ${quantite}`)
 
-
-
+  if (pTotale < 200) {
+    taxe = pTotale * 0.07
+  } else if (pTotale < 500) {
+    taxe = pTotale * 0.12
+  } else taxe = pTotale * 0.15
+  pFinale = pTotale + taxe
+  alert(`le produit ${titre} est de prix initiale ${pTotale} est la taxe serai: ${taxe} avec un prix finale: ${pFinale}`)
 }
