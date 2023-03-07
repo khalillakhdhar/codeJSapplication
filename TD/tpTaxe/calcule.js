d9ee489052c5f142b8c8268bb0795d0bbfde9fa7
khalil <khalillakhdharatc@gmail.com>
2023-03-07 11:27:36 +0100

    Create calcule.js

---
+++ TD/tpTaxe/calcule.js
@@ -0,0 +1,12 @@
+function calculeTaxe()
+{
+    let taxe =0;
+    let titre=document.getElementById("titre").value;
+    let prix=document.getElementById("prix").value;
+    let quantite=document.getElementById("quantite").value;
+
+    alert(`produit: ${titre} and prix: ${prix} and quantite: ${quantite}`);
+console.log(`produit: ${titre} and prix: ${prix} and quantite: ${quantite}`);
+
+
+}
